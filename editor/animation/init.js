fdcf4ce7fc5a7ca81241663b334afc7f9012413e
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-10-16 12:22:18 +1200

    animation

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -77,13 +77,9 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
             }
             //Dont change the code before it
 
-            //Your code here about test explanation animation
-            //$content.find(".explanation").html("Something text for example");
-            //
-            //
-            //
-            //
-            //
+            var canvas = new ShortKnightPathCanvas(checkioInput);
+            canvas.createCanvas($content.find(".explanation")[0]);
+            canvas.startAnimation(explanation);
 
 
             this_e.setAnimationHeight($content.height() + 60);
@@ -108,10 +104,88 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
         var colorGrey1 = "#EBEDED";
 
         var colorWhite = "#FFFFFF";
-        //Your Additional functions or objects inside scope
-        //
-        //
-        //
+
+        function ShortKnightPathCanvas(dataInput) {
+            var zx = 20;
+            var zy = 10;
+            var cellSize = 30;
+            var cellN = 8;
+            var fullSizeX = zx * 1.5 + cellSize * cellN;
+            var fullSizeY = zy * 3 + cellSize * cellN;
+            var delay = 200;
+
+            var colorDark = "#294270";
+            var colorBlue = "#65A1CF";
+            var colorOrange = "#FABA00";
+            var colorDarkOrange = "#FA8F00";
+
+            var letters = "abcdefgh";
+
+            var attrRectWhite = {"stroke": colorDark, "stroke-width": 1.5};
+            var attrRectBlack = {"stroke": colorDark, "stroke-width": 1.5, "fill": colorBlue};
+            var attrRectOrange = {"fill": colorOrange};
+            var attrRectBlackOrange = {"fill": colorDarkOrange};
+
+            var attrText = {"stroke": colorDark, "font-size": 16, "font-family": "Verdana"};
+
+            var paper;
+            var start = [7 - letters.indexOf(dataInput[0]), parseInt(dataInput[1]) - 1];
+            var end = [7 - letters.indexOf(dataInput[3]), parseInt(dataInput[4]) - 1];
+            var knight;
+
+            this.createCanvas = function (dom) {
+                paper = Raphael(dom, fullSizeX, fullSizeY, 0, 0);
+                for (var i = 0; i < cellN; i++) {
+                    for (var j = 0; j < cellN; j++) {
+                        var r = paper.rect(zx + cellSize * j,
+                                zy + cellSize * i,
+                                cellSize,
+                                cellSize).attr(
+                                (i + (j % 2)) % 2 ? attrRectBlack : attrRectWhite
+                            );
+                        if ((i == start[0] && j == start[1]) || (i == end[0] && j == end[1])) {
+                            r.attr((i + (j % 2)) % 2 ? attrRectBlackOrange : attrRectOrange);
+                        }
+                    }
+                    paper.text(zx + cellSize * i + cellSize / 2,
+                        2 * zy + cellSize * cellN,
+                        String(i + 1)
+                    ).attr(attrText);
+                    paper.text(zx / 2,
+                        fullSizeY - zy * 2 - cellSize / 2 - cellSize * i,
+                        letters[i]
+                    ).attr(attrText);
+                }
+//                knight = paper.image("icon-knight.png",
+//                    zx + padding + start[1] * cellSize,
+//                    zy + padding + start[0] * cellSize,
+//                    cellSize - 2 * padding, cellSize - 2 * padding);
+                knight = paper.text(
+                    zx + start[1] * cellSize + cellSize / 2,
+                    zy + start[0] * cellSize + cellSize / 2,
+                    "\u265E").attr({"font-size": 28});
+
+            };
+            this.startAnimation = function (expl) {
+                for (var k = 0; k < expl.length; k++) {
+
+                    setTimeout(function () {
+                        var move = expl[k];
+                        return function () {
+                            knight.animate({"transform": "...t" + cellSize * move[1] + ",0"}, delay * Math.abs(move[1]));
+                        }
+                    }(),
+                        delay * 5 * k);
+                    setTimeout(function () {
+                        var move = expl[k];
+                        return function () {
+                            knight.animate({"transform": "...t0," + (-1) * cellSize * move[0]}, delay * Math.abs(move[0]));
+                        }
+                    }(),
+                        delay * 5 * k + delay * Math.abs(expl[k][1]) + 50);
+                }
+            }
+        }
 
 
     }
