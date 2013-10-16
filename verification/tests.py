045334738dab904aadfe2fbda9713ee10fbcc06f
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-10-16 12:21:08 +1200

    referee and tests

--- verification/tests.py
+++ verification/tests.py
@@ -10,27 +10,27 @@ Each test is dict with
 
 TESTS = {
     "Basics": [
-        {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
-        },
-        {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
+        {"input": "b1-d5", "answer": 2, "explanation": [[1, 2], [1, 2]]},
+        {"input": "a6-b8", "answer": 1, "explanation": [[1, 2]]},
+        {"input": "h1-g2", "answer": 4, "explanation": [[-2, 1], [-2, 1], [2, 1], [1, -2]]},
+        {"input": "h8-d7", "answer": 3, "explanation": [[-1, -2], [-1, 2], [-2, -1]]},
+        {"input": "a1-h8", "answer": 6, "explanation": [[1, 2], [-1, 2], [1, 2], [2, 1], [2, -1], [2, 1]]},
+        {"input": "b1-e8", "answer": 4, "explanation": [[-1, 2], [1, 2], [1, 2], [2, 1]]},
+        {"input": "e2-e3", "answer": 3, "explanation": [[-2, 1], [1, 2], [1, -2]]},
+        {"input": "d5-b1", "answer": 2, "explanation": [[-1, -2], [-1, -2]]},
+        {"input": "a4-h4", "answer": 5, "explanation": [[1, 2], [1, 2], [2, -1], [2, -1], [1, -2]]},
+        {"input": "c3-d5", "answer": 1, "explanation": [[1, 2]]}
     ],
     "Extra": [
-        {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
-        },
-        {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
+        {"input": "b2-d5", "answer": 3, "explanation": [[-1, 2], [1, 2], [2, -1]]},
+        {"input": "a5-b8", "answer": 2, "explanation": [[2, 1], [-1, 2]]},
+        {"input": "h3-g2", "answer": 2, "explanation": [[-2, 1], [1, -2]]},
+        {"input": "h4-d7", "answer": 3, "explanation": [[-1, 2], [-1, 2], [-2, -1]]},
+        {"input": "a2-h8", "answer": 5, "explanation": [[1, 2], [1, 2], [1, 2], [2, -1], [2, 1]]},
+        {"input": "b3-e8", "answer": 4, "explanation": [[-1, 2], [1, 2], [2, -1], [1, 2]]},
+        {"input": "e1-e3", "answer": 2, "explanation": [[-2, 1], [2, 1]]},
+        {"input": "d7-b1", "answer": 4, "explanation": [[2, -1], [-1, -2], [-1, -2], [-2, -1]]},
+        {"input": "a2-h4", "answer": 5, "explanation": [[1, 2], [1, 2], [2, 1], [2, -1], [1, -2]]},
+        {"input": "c2-d5", "answer": 2, "explanation": [[-1, 2], [2, 1]]}
     ]
 }
